b438807c1a00696fd68a69fbd8e849545b64d839
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-31 20:38:16 +0600

    install nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, world')
+    res.send('111 Hello, world')
 });
 
 app.listen(4444, (err) => {
