e5ee500496b6ec869216ceaa5c8d4515d5c46a2c
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-06-02 23:06:14 +0600

    install express validator

--- index.js
+++ index.js
@@ -11,27 +11,10 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send(' Hello, world')
-});
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body)
 
-    if (req.body.email === "test@test.ru") {
-        const token = jwt.sign(
-            {
-            email: req.body.email,
-            fullName: 'Валера Тимкин',
-        },
-            "secret123",
-        );
-    }
-
-    res.json({
-        success: true,
-        token,
-    })
+app.post('/auth/register', (req, res) => {
+   
 });
 
 app.listen(4444, (err) => {
--- models/user.js
+++ models/user.js
@@ -21,4 +21,4 @@ const userSchema = new mongoose.Schema({
     },
 );
 
-export default mongoose.model('User', userSchema)
+export default mongoose.model('User', userSchema);
