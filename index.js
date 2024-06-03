85e1990141a084c4b0a2eef23fc8d75b5fa54f4f
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-06-03 20:39:50 +0600

    creating post auth register

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
 
-import {registerValidator} from './validations/auth.js'
+import { registerValidator } from './validations/auth.js'
 
 mongoose.connect("mongodb+srv://toktobaevrasul2002:wwwwww@cluster0.uppwel6.mongodb.net/",
 
 ).then(() => console.log('Connected to database'))
-   .catch((err) => console.log("Db error", err));
+    .catch((err) => console.log("Db error", err));
 
 const app = express();
 
@@ -15,8 +16,15 @@ app.use(express.json());
 
 
 
-app.post('/auth/register', registerValidator,(req, res) => {
-   
+app.post('/auth/register', registerValidator, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
+
+    res.json({
+        success: true,
+    })
 });
 
 app.listen(4444, (err) => {
