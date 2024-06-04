4ea30b655d89703e36def74a09df4000fe6ee53e
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-06-04 23:24:27 +0600

    refactor in app

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ app.post('/auth/register', registerValidator, async (req, res) => {
     res.json(user);  
 });
 
-// app.listen(4444, (err) => {
-//     if (err) {
-//         return console.log(err);
-//     }
-//     console.log('Server is running on port 3000');
-// });
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Server is running on port 3000');
+});
