c0ec9973610158dcd53ccfde679cfebb854d74f4
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-06-05 21:18:04 +0600

    add folder utils

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { registerValidator } from './validations/auth.js';
 
 
 import UserModel from './models/User.js';
+import checkAuth  from './utils/checkAuth.js';
 
 mongoose.connect("mongodb+srv://toktobaevrasul2002:wwwwww@cluster0.uppwel6.mongodb.net/blog",
 
@@ -24,18 +25,35 @@ app.post('/auth/login', async (req, res) => {
         const user = await UserModel.findOne({ email: req.body.email });
 
         if(!user) {
-            return req.status(404).json({ message: 'Пользователь не найден' });
+            return res.status(404).json({ message: 'Пользователь не найден' });
         }
 
         const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
 
         if(!isValidPass) {
-            return req.status(400).json({ message: 'Неверный пароль' });
+            return res.status(400).json({ message: 'Неверный логин или пароль' });
         }
-    } catch (err) {
 
+        const token = jwt.sign({
+            _id: user._id,
+        },
+            'secret123',
+            {
+                expiresIn: "30days",
+            }
+        );
+        const { passwordHash,...userData } = user._doc;
+
+        res.json({
+           ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Что-то пошло не так' });
     }
 })
+
 
 app.post('/auth/register', registerValidator, async (req, res) => {
     try {
@@ -79,6 +97,11 @@ app.post('/auth/register', registerValidator, async (req, res) => {
         });
     }
 });
+
+
+app.get("/auth/me", (req, res) => {
+
+})
 
 app.listen(4444, (err) => {
     if (err) {
