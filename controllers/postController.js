745447f740209a003679d5c298d0a8106f9896e8
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-06-10 21:12:25 +0600

    writing functionality for articles

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,23 @@
-export const create = (req, res) => {
-    try {
-            
-    } catch (err) {
+import PostModel from "../models/post";
 
+
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId,
+        });
+
+        const post = await doc.save();
+
+        res.json(post);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Ошибка при созданий статьи",
+        });
     }
 }
