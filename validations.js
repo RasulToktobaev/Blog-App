5d69ab943edb106bde805985a808b5aca0f01b89
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-06-10 20:37:33 +0600

    add new file in controller

--- validations.js
+++ validations.js
@@ -1,5 +1,10 @@
 import { body } from "express-validator";
 
+export const loginValidator = [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+];
+
 export const registerValidator = [
     body('email').isEmail(),
     body('password').isLength({ min: 5 }),
