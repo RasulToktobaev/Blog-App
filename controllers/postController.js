db03e4daba50b22eabd77331a085b4a64cf66940
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-06-13 23:12:22 +0600

    add title in new PostModel

--- controllers/postController.js
+++ controllers/postController.js
@@ -84,7 +84,7 @@ export const getOne = async (req, res) => {
 export const create = async (req, res) => {
     try {
         const doc = new PostModel({
-            
+            title: req.body.title,
             text: req.body.text,
             imageUrl: req.body.imageUrl,
             tags: req.body.tags,
