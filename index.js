7c9265045944bf2de9cba394f21d38cfb2bd8fda
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-07-01 21:50:25 +0600

    add middleware , handleValidationErrors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { registerValidator, loginValidator, postCreateValidation } from './valid
 import checkAuth from './utils/checkAuth.js';
 import * as userController from './controllers/userController.js';
 import * as postController from './controllers/postController.js';
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 
 mongoose.connect("mongodb+srv://toktobaevrasul2002:wwwwww@cluster0.uppwel6.mongodb.net/blog")
     .then(() => console.log('Connected to database'))
@@ -24,10 +25,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
-app.use('/uploads', express.static('uploads')); 
+app.use('/uploads', express.static('uploads'));
 
-app.post('/auth/login', loginValidator, userController.login);
-app.post('/auth/register', registerValidator, userController.register);
+app.post('/auth/login', handleValidationErrors, loginValidator, userController.login);
+app.post('/auth/register', handleValidationErrors, registerValidator, userController.register);
 app.get("/auth/me", checkAuth, userController.getMe);
 
 app.post("/upload", checkAuth, upload.single('image'), (req, res) => {
@@ -40,7 +41,7 @@ app.get("/posts", postController.getAll);
 app.get("/posts/:id", postController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, postController.create);
 app.delete("/posts/:id", checkAuth, postController.remove);
-app.patch("/posts/:id", checkAuth, postController.update);
+app.patch("/posts/:id", checkAuth, postCreateValidation, postController.update);
 
 app.listen(4444, (err) => {
     if (err) {
